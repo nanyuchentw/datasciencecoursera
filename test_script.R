c08d74d01c5e60cc0dedc981f986974ef3a91a88
nanyuchentw <nelly.chen@gmail.com>
2023-03-17 05:50:17 +0800

    added part3 scripts for test

--- test_script.R
+++ test_script.R
@@ -10,3 +10,25 @@ complete("specdata", 1)
 complete("specdata", c(2, 4, 8, 10, 12))
 complete("specdata", 30:25)
 complete("specdata", 3)
+
+#part3
+source("corr.R")
+source("complete.R")
+cr <- corr("specdata", 150)
+head(cr)
+summary(cr)
+
+cr <- corr("specdata", 400)
+head(cr)
+summary(cr)
+
+cr <- corr("specdata", 5000)
+summary(cr)
+length(cr)
+
+cr <- corr("specdata")
+summary(cr)
+length(cr)
+
+
+
