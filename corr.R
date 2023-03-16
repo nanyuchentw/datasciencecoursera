d7547acadd01968d527bff56d2cf7db1feee063a
nanyuchentw <nelly.chen@gmail.com>
2023-03-17 05:51:14 +0800

    added the cor calculation with complete cases

--- corr.R
+++ corr.R
@@ -1,4 +1,18 @@
 
 corr <- function (directory, threshold= 0){
-    
+        result <- vector()
+        df <- complete(directory)
+        selected_df <- df[ (df$nobs) >threshold, ]
+        if (is.null(selected_df)){
+                nrow(selected_df)
+        } else {
+                for (x in 1:nrow(selected_df)){
+                        y <-formatC(selected_df[x, 1], width = 3, format = "d", flag = "0")
+                        filename <- paste(directory, "/", y, ".csv", sep="")
+                        each_df <- read.csv(filename)
+                        clean_data <- each_df[complete.cases(each_df), ]
+                        result[x] <- cor(clean_data$sulfate, clean_data$nitrate)
+                        result
+                }
+        }
 }
