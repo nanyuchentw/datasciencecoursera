6fe5262bf39c054ed659a3070a307ee4b36f3c37
nanyuchen <nelly.chen@gmail.com>
2023-03-17 09:19:55 +0800

    finished corr.R code

--- corr.R
+++ corr.R
@@ -1,18 +1,19 @@
 
 corr <- function (directory, threshold= 0){
-        result <- vector()
+        result <- numeric()
         df <- complete(directory)
         selected_df <- df[ (df$nobs) >threshold, ]
-        if (is.null(selected_df)){
-                nrow(selected_df)
+        if (nrow(selected_df) == 0){
+            result
         } else {
-                for (x in 1:nrow(selected_df)){
-                        y <-formatC(selected_df[x, 1], width = 3, format = "d", flag = "0")
-                        filename <- paste(directory, "/", y, ".csv", sep="")
-                        each_df <- read.csv(filename)
-                        clean_data <- each_df[complete.cases(each_df), ]
-                        result[x] <- cor(clean_data$sulfate, clean_data$nitrate)
-                        result
+            for (x in 1:nrow(selected_df)){
+                y <-formatC(selected_df[x, 1], width = 3, format = "d", flag = "0")
+                filename <- paste(directory, "/", y, ".csv", sep="")
+                each_df <- read.csv(filename)
+                clean_data <- each_df[complete.cases(each_df), ]
+                result[x] <- cor(clean_data$sulfate, clean_data$nitrate)
                 }
+        
         }
+        result
 }
