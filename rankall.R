4218b4d9fcc7bcbe4e4c1d5e8c3e01879c8dd21d
nanyuchen <nelly.chen@gmail.com>
2023-03-21 08:19:32 +0800

    data frame selected sorted. not yet rbind the results

--- rankall.R
+++ rankall.R
@@ -1,7 +1,7 @@
 
 rankall <- function (outcome, num) {
         library(dplyr)
-        result.df <- data.frame("hospital"=NULL, "state"=NULL)
+        result.df <- data.frame()
         df <- read.csv("Assignment3/outcome-of-care-measures.csv")
         suppressWarnings(df[, 11] <- as.numeric(df[,11]))
         suppressWarnings(df[, 17] <- as.numeric(df[,17]))
@@ -13,46 +13,41 @@ rankall <- function (outcome, num) {
                 state_vec <- unique(df[,7])
                 if (outcome == "heart attack"){
                         for (i in state_vec){
-                                state_df <- df[df[,7]== state_vec[i] ,]
+                                state_df <- df[df[,7]== i ,]
                                 clean_df <- state_df[complete.cases(state_df[,11]), ]
                                 n_in_state <- nrow(clean_df)
-                                if (num== "best") {num <-1}
+                                if (num== "best") {num <- 1}
                                 if (num== "worst"){num <- n_in_state} 
                                 state_result <- arrange(clean_df, clean_df[,11], clean_df[,2])
-                                rbind (result.df, c(state_result[num, 2], i))
-                        }
+                                new_data <- data.frame(hospital=state_result[num, 2], state=i)
+                                rbind (result.df, new_data)
+                                }
                 } else if (outcome== "heart failure"){
                         for (i in state_vec){
-                                state_df <- df[df[,7]== state_vec[i] ,]
+                                state_df <- df[df[,7]== i ,]
                                 clean_df <- state_df[complete.cases(state_df[,17]), ]
                                 n_in_state <- nrow(clean_df)
                                 if (num== "best") {num <-1}
                                 if (num== "worst"){num <- n_in_state}
                                 state_result <- arrange(clean_df, clean_df[,17], clean_df[,2])
-                                rbind (result.df, c(state_result[num, 2], i))
+                                new_data <- data.frame(hospital=state_result[num, 2], state=i)
+                                rbind (result.df, new_data)
                         }
                 } else {
                         for (i in state_vec){
-                                state_df <- df[df[,7]== state_vec[i] ,]
+                                state_df <- df[df[,7]== i ,]
                                 clean_df <- state_df[complete.cases(state_df[,23]), ]
                                 n_in_state <- nrow(clean_df)
                                 if (num== "best") {num <-1}
                                 if (num== "worst"){num <- n_in_state} 
                                 state_result <- arrange(clean_df, clean_df[,23], clean_df[,2])
-                                rbind (result.df, c(state_result[num, 2], i))
+                                new_data <- data.frame(hospital=state_result[num, 2], state=i)
+                                rbind (result.df, new_data)
                         }
                 }
-                result.df
+               
         }
+        result.df
 }
 
 
-num <- 10
-for (i in 1:length(state_vec)){
-        state_df <- df[df[,7]== state_vec[i] ,]
-        clean_df <- state_df[complete.cases(state_df[,11]), ]
-        n_in_state <- nrow(clean_df)
-        state_result <- arrange(clean_df, clean_df[,11], clean_df[,2])
-        rbind (result.df, c(state_result[num, 2], i))
-        }
-result.df
