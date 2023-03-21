ffcd3c0bfebc24a8f30f1430a008665bc406c3c0
nanyuchen <nelly.chen@gmail.com>
2023-03-21 13:46:24 +0800

    testing scripts for assigment3

--- scripts for assignment3.R
+++ scripts for assignment3.R
@@ -26,3 +26,22 @@ rankhospital("NY", "hart failure", 5)
 #part4
 source("rankall.R")
 head(rankall("heart attack", 20), 10)
+tail(rankall('pneumonia', 'worst'),3)
+tail(rankall('heart failure'), 10)
+
+
+result.df <- data.frame()
+num <- 3
+for (i in c("UT")){
+    state_df <- df[df[,7]== i ,]
+    clean_df <- state_df[complete.cases(state_df[,17]), ]
+    n_in_state <- nrow(clean_df)
+    state_result <- arrange (clean_df, clean_df[,17])
+    new_data <- data.frame(hospital=state_result[num, 2], state=i)
+    result.df <-rbind (result.df, new_data)
+    }
+result.df
+
+tail(state_result)
+k <- state_result[ ,c(2, 17)]
+k
