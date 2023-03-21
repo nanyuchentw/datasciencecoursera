9f02058fd96a09064b96cbf8e7df283db1d5f68d
nanyuchentw <nelly.chen@gmail.com>
2023-03-22 01:40:32 +0800

    fixed typo in the "pneumonia" part. Now working properly.

--- rankhospital.R
+++ rankhospital.R
@@ -4,7 +4,7 @@ rankhospital <- function (state, outcome, num=0){
         suppressWarnings(df[, 11] <- as.numeric(df[,11]))
         suppressWarnings(df[, 17] <- as.numeric(df[,17]))
         suppressWarnings(df[, 23] <- as.numeric(df[,23]))
-        
+        temp <- numeric()
         if (!state %in% df[, 7]){
                 stop("invalid state") 
         } else {
@@ -14,33 +14,48 @@ rankhospital <- function (state, outcome, num=0){
                 } else {
                         if(outcome =="heart attack") {
                                 n_attack <- length(df[, 11] [complete.cases(df[,11])])
-                                if (num=="best") {num <-1}
-                                if (num== "worst") {num <- n_attack}
+                                if (num=="best") {
+                                        temp <-1
+                                } else if (num == "worst"){
+                                        temp <- n_attack        
+                                } else {
+                                        temp <- num
+                                }
                                 a_df <- df[, c(2, 11)]
                                 a_df <- a_df[complete.cases(a_df),]
                                 names(a_df) <- c("hospital", "attack")
                                 result_df <- arrange(a_df, attack, hospital)
-                                print(result_df[num, 1])
+                                print(result_df[temp, 1])
                                 
                         } else if (outcome =="heart failure"){
                                 n_failure <- length (df[, 17] [complete.cases(df[,17])])
-                                if (num=="best") {num <-1}
-                                if (num== "worst"){num <- n_failure}
+                                if (num=="best") {
+                                        temp <-1
+                                } else if(num== "worst"){
+                                        temp <- n_failure
+                                } else {
+                                        temp <-num        
+                                        }
                                 f_df <- df[, c(2, 17)]
                                 f_df <- f_df[complete.cases(f_df),]
                                 names(f_df) <- c("hospital", "failure")
                                 result_df <- arrange(f_df, failure, hospital)
-                                print(result_df[num, 1])
+                                print(result_df[temp, 1])
                                 
                         } else {
                                 n_pneumonia <- length (df[, 23] [complete.cases(df[,23])])
-                                if (num=="best") {num <-1}
-                                if (num== "worst"){num <- n_pneumonia} 
-                                p_df <- df[, c(2, 17)]
+                                if (num=="best") {
+                                        temp <-1
+                                } else if (num =="worst") {
+                                        temp <- n_pneumonia        
+                                } else {
+                                        temp <- num        
+                                        }
+                                p_df <- df[, c(2, 23)]
                                 p_df <- p_df[complete.cases(p_df),]
                                 names(p_df) <- c("hospital", "pneumonia")
                                 result_df <- arrange(p_df, pneumonia, hospital)
-                                print(result_df[num, 1])
+                                print(result_df[temp, 1])
                                 }
                         }
                 }        
